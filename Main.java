23cd393bca3b5d9033d3683bb7852b2dca5c87d5
Amman <amman.moini@gmail.com>
2023-09-23 01:43:47 +0530

    First Commit

---
+++ Main.java
@@ -0,0 +1,46 @@
+//import java.util.Scanner;
+//public class Main {
+//    public static void main(String[] args)
+//    {
+//        for(int i =100; i<1000; i++)
+//        {
+//            if (isArmstrong(i))
+//            {
+//                System.out.println(i + " ");
+//
+//            }
+//        }
+//
+//    }
+//
+//    static boolean isArmstrong(int n) {
+//        int original = n;
+//        int sum = 0;
+//
+//        while (n > 0) {
+//            int rem = n % 10;
+//            n = n / 10;
+//            sum = sum + (rem * rem * rem);
+//        }
+//        return sum == original;
+//    }
+//
+//}
+//
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+    }
+
+    public static void checkPrime(int num)
+    {
+
+    }
+}
+
+
